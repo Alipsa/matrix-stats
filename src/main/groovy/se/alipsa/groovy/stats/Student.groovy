3076ccfaa569256d34215239cbc2493b114809f9
per <per@alipsa.se>
2023-05-30 23:26:02 +0300

    implement one sample t test

--- src/main/groovy/se/alipsa/groovy/stats/Student.groovy
+++ src/main/groovy/se/alipsa/groovy/stats/Student.groovy
@@ -1,5 +1,7 @@
 package se.alipsa.groovy.stats
 
+import org.apache.commons.math3.stat.inference.TTest
+import se.alipsa.groovy.matrix.ListConverter
 import se.alipsa.groovy.matrix.Stat
 
 import java.math.RoundingMode
@@ -28,21 +30,102 @@ import java.math.RoundingMode
  */
 class Student {
 
-  static Result twoSample(List<? extends Number> first, List<? extends Number> second) {
+  /**
+   * An Independent Samples t-test compares the means for two groups.
+   * @param first
+   * @param second
+   * @param equalVariance
+   * @return
+   */
+  static Result tTest(List<? extends Number> first, List<? extends Number> second, Boolean equalVariance = null) {
     BigDecimal mean1 = Stat.mean(first)
     BigDecimal mean2 = Stat.mean(second)
-    BigDecimal sd1 = Stat.sd(first)
-    BigDecimal sd2 = Stat.sd(second)
+    BigDecimal var1 = Stat.variance(first, true)
+    BigDecimal var2 = Stat.variance(second, true)
+    BigDecimal sd1 = Math.sqrt(var1 as double)
+    BigDecimal sd2 = Math.sqrt(var2 as double)
+    Integer n1 = first.size()
+    Integer n2 = second.size()
     Result result = new Result()
-    def t = (mean1 - mean2) / Math.sqrt((sd1 * sd1) / first.size() + (sd2 * sd2) / second.size())
+    def t = (mean1 - mean2) / Math.sqrt((sd1 ** 2) / n1 + (sd2 ** 2) / n2)
     result.tVal = t
+    // To be sure, perform an F test test to check, 4 is general rule of thumb, https://www.statology.org/equal-variance-assumption/
+    boolean isEqualVariance
+    if (equalVariance == null) {
+      isEqualVariance = (var1 - var2).abs() < 4
+    } else {
+      isEqualVariance = equalVariance
+    }
+    if (isEqualVariance) {
+      result.df = n1 + n2 - 2
+      result.description = "Welch two sample t-test with equal variance"
+    } else {
+      def dividend = (var1/n1 + var2/n2) ** 2
+      def divisor1 = (var1 ** 2) / ((n1 ** 2) * (n1-1))
+      def divisor2 = (var2 ** 2) / ((n2 ** 2) * (n2 -1))
+      result.df =  dividend / (divisor1 + divisor2)
+      result.description = "Welch two sample t-test with unequal variance"
+    }
+    result.mean1 = mean1
+    result.mean2 = mean2
+    result.var1 = var1
+    result.var2 = var2
+    result.sd1 = sd1
+    result.sd2 = sd2
+    result.n1 = n1
+    result.n2 = n2
+    // TODO: implement p value function, relying on commons math3 as a temporary solution
+    TTest tTest = new TTest()
+    result.pVal = tTest.tTest(ListConverter.toDoubleArray(first), ListConverter.toDoubleArray(second))
+    return result
+  }
+
+  /**
+   * A One sample t-test tests the mean of a single group against a known mean.
+   * t= (m−μ) / s/sqrt(n)
+   * where
+   * <ul>
+   *  <li>m is the sample mean</li>
+   *  <li>n is the sample size</li>
+   *  <li>s is the sample standard deviation with n−1 degrees of freedom</li>
+   *  <li>μ is the theoretical mean value</li>
+   * </ul>
+   */
+  static SingleResult tTest(List<? extends Number> values, Number comparison) {
+    def mean = Stat.mean(values)
+    def variance = Stat.variance(values)
+    def sd = Math.sqrt(variance as double)
+    def n = values.size()
+    def dividend = mean - comparison
+    def divisor = sd / Math.sqrt(n)
+    SingleResult result = new SingleResult("One Sample t-test")
+    result.tVal = dividend / divisor
+    result.mean = mean
+    result.var = variance
+    result.sd = sd
+    result.n = n
+    result.df = n -1
+    // TODO: implement p value function, relying on commons math3 as a temporary solution
+    TTest tTest = new TTest()
+    result.pVal = tTest.tTest(comparison.doubleValue(), ListConverter.toDoubleArray(values))
     return result
   }
 
 
 
   static class Result {
+    Integer n1
+    Integer n2
+    BigDecimal mean1
+    BigDecimal mean2
+    BigDecimal var1
+    BigDecimal var2
+    BigDecimal sd1
+    BigDecimal sd2
     BigDecimal tVal
+    BigDecimal pVal
+    BigDecimal df
+    String description
 
     BigDecimal getT() {
       return tVal
@@ -50,6 +133,169 @@ class Student {
 
     BigDecimal getT(int numberOfDecimals) {
       return getT().setScale(numberOfDecimals, RoundingMode.HALF_EVEN)
+    }
+
+    BigDecimal getP() {
+      return pVal
+    }
+
+    BigDecimal getP(int numberOfDecimals) {
+      return getP().setScale(numberOfDecimals, RoundingMode.HALF_EVEN)
+    }
+
+    BigDecimal getDf() {
+      return df
+    }
+
+    BigDecimal getDf(int numberOfDecimals) {
+      return getDf().setScale(numberOfDecimals, RoundingMode.HALF_EVEN)
+    }
+
+    BigDecimal getMean1() {
+      return mean1
+    }
+
+    BigDecimal getMean1(int numberOfDecimals) {
+      return getMean1().setScale(numberOfDecimals, RoundingMode.HALF_EVEN)
+    }
+
+    BigDecimal getMean2() {
+      return mean2
+    }
+
+    BigDecimal getMean2(int numberOfDecimals) {
+      return getMean2().setScale(numberOfDecimals, RoundingMode.HALF_EVEN)
+    }
+
+    BigDecimal getVar1() {
+      return var1
+    }
+
+    BigDecimal getVar1(int numberOfDecimals) {
+      return getVar1().setScale(numberOfDecimals, RoundingMode.HALF_EVEN)
+    }
+
+    BigDecimal getVar2() {
+      return var2
+    }
+
+    BigDecimal getVar2(int numberOfDecimals) {
+      return getVar2().setScale(numberOfDecimals, RoundingMode.HALF_EVEN)
+    }
+
+    BigDecimal getSd1() {
+      return sd1
+    }
+
+    BigDecimal getSd1(int numberOfDecimals) {
+      return getSd1().setScale(numberOfDecimals, RoundingMode.HALF_EVEN)
+    }
+
+    BigDecimal getSd2() {
+      return sd2
+    }
+
+    BigDecimal getSd2(int numberOfDecimals) {
+      return getSd2().setScale(numberOfDecimals, RoundingMode.HALF_EVEN)
+    }
+
+    Integer getN1() {
+      return n1
+    }
+
+    Integer getN2() {
+      return n2
+    }
+
+    String getDescription() {
+      return description
+    }
+
+    @Override
+    String toString() {
+      return """
+      ${getDescription()}
+      t = ${getT(3)}, df = ${getDf(3)}, p = ${getP(3)}
+      x: mean = ${getMean1(3)}, size = ${getN1()}, sd = ${getSd1(3)}
+      y: mean = ${getMean2(3)}, size = ${getN2()}, sd = ${getSd2(3)} 
+      """.stripIndent()
+    }
+  }
+
+  static class SingleResult {
+    Integer n
+    BigDecimal mean
+    BigDecimal var
+    BigDecimal sd
+    BigDecimal tVal
+    BigDecimal pVal
+    Integer df
+    String description
+
+    SingleResult(String description) {
+      this.description = description
+    }
+
+    BigDecimal getT() {
+      return tVal
+    }
+
+    BigDecimal getT(int numberOfDecimals) {
+      return getT().setScale(numberOfDecimals, RoundingMode.HALF_EVEN)
+    }
+
+    BigDecimal getP() {
+      return pVal
+    }
+
+    BigDecimal getP(int numberOfDecimals) {
+      return getP().setScale(numberOfDecimals, RoundingMode.HALF_EVEN)
+    }
+
+    Integer getDf() {
+      return df
+    }
+
+    BigDecimal getMean() {
+      return mean
+    }
+
+    BigDecimal getMean(int numberOfDecimals) {
+      return getMean().setScale(numberOfDecimals, RoundingMode.HALF_EVEN)
+    }
+
+
+    BigDecimal getVar() {
+      return var
+    }
+
+    BigDecimal getVar(int numberOfDecimals) {
+      return getVar().setScale(numberOfDecimals, RoundingMode.HALF_EVEN)
+    }
+
+    BigDecimal getSd() {
+      return sd
+    }
+
+    BigDecimal getSd(int numberOfDecimals) {
+      return getSd().setScale(numberOfDecimals, RoundingMode.HALF_EVEN)
+    }
+
+    Integer getN() {
+      return n
+    }
+
+    String getDescription() {
+      return description
+    }
+
+    @Override
+    String toString() {
+      return """
+      ${getDescription()}
+      t = ${getT(3)}, df = ${getDf()}, p = ${getP(3)}
+      mean = ${getMean(3)}, size = ${getN()}, sd = ${getSd(3)}
+      """.stripIndent()
     }
   }
 }
--- src/test/groovy/StudentTest.groovy
+++ src/test/groovy/StudentTest.groovy
@@ -12,6 +12,12 @@ class StudentTest {
    * flowers <- iris[iris$Species == 'setosa' | iris$Species == 'virginica',]
    * print(t.test(Petal.Length ~ Species, data = flowers))
    * </pre></code>
+   * Another way to express the same thing is:
+   * <code><pre>
+   * setosa <- iris[iris$Species == 'setosa',]
+   * virginica <- iris[iris$Species == 'virginica',]
+   * print(t.test(setosa[, 'Petal.Length'], virginica[, 'Petal.Length']))
+   * </pre></code>
    */
   @Test
   void testTwoSample() {
@@ -23,7 +29,45 @@ class StudentTest {
     def virginica = iris.subset {
       it[speciesIdx] == 'virginica'
     }
-    Student.Result result = Student.twoSample(setosa['Petal Length'], virginica['Petal Length'])
-    assertEquals(-49.96570336, result.getT(8))
+    Student.Result result = Student.tTest(setosa['Petal Length'], virginica['Petal Length'], false)
+    println(result)
+    assertEquals(-49.98618626, result.getT(8), "t value")
+    println("var1 = ${result.var1}, var2 = ${result.var2}")
+    // The 8:th decimal differs from gnu R but matches e.g. https://calculator-online.net/degrees-of-freedom-calculator/
+    assertEquals(58.60939454, result.getDf(8), "Degrees of freedom")
+    assertEquals(0.17366400, result.getSd1(8), "sd1")
+    assertEquals(0.55189470, result.getSd2(8), "sd2")
+    assertEquals(0.55189470, result.getSd2(8), "sd2")
+    assertEquals(9.269628E-50, result.p, 0.0000001)
+  }
+
+  /**
+   * Equivalent in R
+   * <code><pre>
+   * plantHeights <- c(14, 14, 16, 13, 12, 17, 15, 14, 15, 13, 15, 14)
+   * print(t.test(plantHeights, mu=15))
+   *
+   * > One Sample t-test
+   * >
+   * > data:  plantHeights
+   * > t = -1.6848, df = 11, p-value = 0.1201
+   * > alternative hypothesis: true mean is not equal to 15
+   * > 95 percent confidence interval:
+   * > 13.46244 15.20423
+   * > sample estimates:
+   * > mean of x
+   * > 14.33333
+   * </pre></code>
+   */
+  @Test
+  void testOneSample() {
+    def plantHeights = [14, 14, 16, 13, 12, 17, 15, 14, 15, 13, 15, 14]
+    def t = Student.tTest(plantHeights, 15)
+    println(t)
+    assertEquals(-1.68484708, t.getT(8))
+    assertEquals(11, t.getDf())
+    assertEquals(0.12014461, t.getP(8))
+    assertEquals(14.33333333, t.getMean(8))
+    assertEquals(1.87878788, t.getVar(8))
   }
 }
